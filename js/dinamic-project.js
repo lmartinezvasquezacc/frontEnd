371a7e901695372c950fa48ccc9d769f3d62f5c9
Martinez Vasquez <l.martinez.vasquez@accenture.com>
2018-09-03 15:56:18 -0300

    primer commit

---
+++ js/dinamic-project.js
@@ -0,0 +1,21 @@
+var addProject = function(event){
+	event.preventDefault();
+	var nuevaFilaProjectTemp = document.getElementById('nuevaFilaProject').innerHTML;
+	
+	var ultNro = document.querySelector('table#tableProjects tbody tr:last-child th').innerHTML;
+	ultNro = parseInt(ultNro)+1;
+	document.querySelector('table#tableProjects tbody').innerHTML += nuevaFilaProjectTemp;
+	document.querySelector('table#tableProjects tbody tr:last-child th').innerHTML = ultNro;	
+};
+var agregarProjectBtn = document.querySelector('#addProject');
+agregarProjectBtn.addEventListener('click', addProject);
+
+$(document ).click(function() {
+  $( "#toggle" ).toggle( "slide" );
+});
+
+$(document).ready(function(){
+    $("#flip").click(function(){
+        $("#panel").slideDown("slow");
+    });
+});
